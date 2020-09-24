b690cee1c04783e14a5eead54359013398c5387c
Ed Ignot <edignot@gmail.com>
2020-09-23 23:10:49 -0600

    Mortgage calculation

--- src/com/mortagecalculator/Main.java
+++ src/com/mortagecalculator/Main.java
@@ -5,14 +5,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        final byte MONTH_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter loan amount");
         int amount = scanner.nextInt();
+
         System.out.print("Enter annual interest rate");
-        float interest = scanner.nextFloat();
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTH_IN_YEAR;
+
         System.out.print("Enter loan period");
         byte years = scanner.nextByte();
-        float morgage = amount * (interest * (1 + interest)) / ((1 + interest) - 1);
-        System.out.print(morgage);
+        int numberOfPayments = years * MONTH_IN_YEAR;
+
+        double mortgage = amount * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1) ;
+        System.out.print(mortgage);
     }
 }
